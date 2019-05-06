1f6b0ec89e67c2c4fc3b6f90542de85264307761
Mahmood Al Rayhan <rayhan.cse12@gmail.com>
2019-05-06 21:48:01 +0600

    cleaned code

--- dbops/create.go
+++ dbops/create.go
@@ -3,47 +3,17 @@ package dbops
 
 import (
     "fmt"
-    // "errors"
-
-    // "github.com/jinzhu/gorm"
-    // _ "github.com/jinzhu/gorm/dialects/postgres"
 
     "github.com/mhmdryhn/gocrud/models"
-    // "github.com/mhmdryhn/gocrud/dbops"
     "github.com/mhmdryhn/gocrud/validators/schemas"
 )
 
 
 func CreateNewAuthor(data map[string]interface{}) (map[string]interface{}, error) {
     var resp map[string]interface{}
-    // var err error
     resp = make(map[string]interface{})
     author := models.Author{}
     resp, err := schemas.ValidateNewAuthor(data)
-
-    // if value, ok := data["name"]; ok {
-    //     author.Name = value.(string)
-    // } else {
-    //     resp["status"] = "name key is missing"
-    // }
-    // if value, ok := data["email"]; ok {
-    //     author.Name = value.(string)
-    // } else {
-    //     resp["status"] = "email key is missing"
-    // }
-    // if value, ok := data["phone"]; ok {
-    //     author.Name = value.(string)
-    // }
-    // if value, ok := data["age"]; ok {
-    //     author.Name = value.(uint)
-    // }
-    // if value, ok := data["address"]; ok {
-    //     author.Name = value.(string)
-    // }
-    //
-    // if r, ok := resp["status"]; ok {
-    //     return resp
-    // }
 
     if err != nil {
         return resp, err
@@ -72,8 +42,6 @@ func CreateNewAuthor(data map[string]interface{}) (map[string]interface{}, error
             return resp, nil
         }
     }
-
-    // db.Create(&user)
 
     return resp, nil
 }
